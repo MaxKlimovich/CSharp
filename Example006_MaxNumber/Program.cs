51f97697cfcf718b2966c15cc8cbb1def6c44bf5
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-04 18:27:31 +0300

    Example 006 maxNumber

---
+++ Example006_MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 2;
+int c = 3;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) a = max;
+if (b > max) b = max;
+if (c > max) c = max;
+if (d > max) d = max;
+if (e > max) e = max;
+
+Console.Write("max = ");
+
+Console.WriteLine(max);

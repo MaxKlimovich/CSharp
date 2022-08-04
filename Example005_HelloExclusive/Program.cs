e4be0e34f22b4c2531a6d1b71e3e0cfa4c054019
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-04 17:37:15 +0300

    Example005 Hello user exclusive:

---
+++ Example005_HelloExclusive/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("О, это же Маша!");
+}
+else 
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}

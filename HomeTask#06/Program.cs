14ecafc0e50b60bba5c964b201ab504e46ed0bbf
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-06 20:29:18 +0300

    add Home task #6

---
+++ HomeTask#06/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrt = (number % 2);
+
+if (sqrt == 0)
+{
+    Console.WriteLine($"Число {number} является четным числом ");
+}
+else 
+{
+    Console.WriteLine($"Число {number} является не четным числом ");
+}
+

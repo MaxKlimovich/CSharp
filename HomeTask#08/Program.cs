9fa13cea14d3b3e09eaf8a6a67651d5d4fbe4a51
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-07 10:51:31 +0300

    Add home Task 008 random

---
+++ HomeTask#08/Program.cs
@@ -0,0 +1,14 @@
+﻿ byte num = 1;
+            while (num <= 40)
+            {
+                int result = num % 2;
+                if (result != 1)
+                {
+                    Console.WriteLine("четное " + num);
+                    num++;
+                }
+                else
+                {
+                    num++;
+                }
+            }

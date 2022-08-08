5fc0f03bea47948d20a55d450e803b5cd5ab4a85
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-08 17:11:47 +0300

    add Expl 010 SecondArray

---
+++ Example010_SecondArray/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] array = { 22, 44, 55, 36, 23, 84, 55, 22, 42 };
+
+int n = array.Length; // смотреть по всей длинне массива.
+int find = 42;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // означает прерывание.
+    }
+index++;
+}
+

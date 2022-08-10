238901d22b8754316f780fbac64521af1a129f7f
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-10 12:09:05 +0300

    add Practice 004 Random Task

---
+++ Practice#004_RandomTask01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 100;
+    int tindlyDigit = num % 10;
+    int result = firstDigit * 10 + tindlyDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+
+int res = MaxDigit(number); 
+Console.WriteLine($"Сгенерированное число {number} => {res}");

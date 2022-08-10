143d9132932d7c633975e62e8eb4db3cb383c65f
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-10 22:18:11 +0300

    add WeekDay Home task #15

---
+++ HomeTask#15/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("введите число от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+string day = "";
+if (num >= 1 && num <= 7)
+{
+    switch(num)
+    {
+        case 1:
+            day = "№1 Понедельник не выходной";
+            break;
+        case 2:
+            day = "№2 Вторник не выходной";
+            break;
+        case 3:
+            day = "№3 Среда не выходной";
+            break;
+        case 4:
+            day = "№4 Четверг не выходной";
+            break;
+        case 5:
+            day = "№5 Пятница не выходной";
+            break;
+        case 6:
+            day = "№6 Суббота выходной";
+            break;
+        case 7:
+            day = "№7 Воскресение выходной";
+            break;
+    }
+    Console.WriteLine("День недели: " +day);
+}
+else
+{
+    Console.WriteLine("Вы ввели не корректное число");
+}
+

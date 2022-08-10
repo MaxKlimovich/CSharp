e38c7d0e9364e7d2b6468747273a4fa0fb33e35d
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-10 10:15:17 +0300

    add Practice 003 massive

---
+++ Practice#003_Massive/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int num = new Random().Next(10, 100);
+
+Console.WriteLine($"случайное число из отрезка 10 - 99 => {num}");
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+
+
+if (secondDigit == firstDigit)
+Console.WriteLine("числа равны");
+else 
+{
+    // int max = fitstDigit;
+    // if (secondDigit > fitstDigit) max = secondDigit;
+    int max = firstDigit > secondDigit ? firstDigit : secondDigit;   
+
+    Console.Write($"наибольшее количество числа {num} => ");
+    Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+    // Console.WriteLine($"наибольшее количество числа {num} => {max}");
+}
+    

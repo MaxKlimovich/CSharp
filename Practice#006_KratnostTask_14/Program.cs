66ad8882165a79090064af95c2405c5afcd84579
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-10 13:56:57 +0300

    add task 14 amd 16 kratnost

---
+++ Practice#006_KratnostTask_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+Console.WriteLine("введите первое число ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+
+bool CheckNum(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+bool result = CheckNum(firstNum);
+
+Console.WriteLine(result ? "yes" : "no");
---
+++ Practice#007_KratnostTask_16/Program.cs
@@ -0,0 +1,22 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+
+Console.WriteLine("введите первое число ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+bool CheckNum (int number1, int number2)
+{
+    return  number1 * number1 == number2 || number2 % number2 == number1;
+}
+
+bool result = CheckNum(firstNum, secondNum);
+
+Console.WriteLine(result ? "yes" : "no");

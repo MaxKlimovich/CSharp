e95243d76a1dc9ba97f64924c1f4cd2e6d137175
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-11 11:12:40 +0300

    add metod Practice #008 Chetverti

--- Practice#008_Chetverti/Program.cs
+++ Practice#008_Chetverti/Program.cs
@@ -11,23 +11,37 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if (x > 0 && y > 0)
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("Первая четверть");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("Вторая четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("Третья четверть");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("Четвертая четверть");
+// }
+// else
+// {
+//     Console.WriteLine("Введены не корректные координаты");
+// }
+
+// Решаем с помощью метода
+
+string Quarter(int xc, int yc)
 {
-    Console.WriteLine("Первая четверть");
+    if (xc > 0 && yc > 0) return ("Первая четверть");
+    if (xc < 0 && yc > 0) return ("Вторая четверть");
+    if (xc < 0 && yc < 0) return ("Третья четверть");
+    if (xc > 0 && yc < 0) return ("Четвертая четверть");
+    return ("Введены не корректные координаты");
 }
-else if (x < 0 && y > 0)
-{
-    Console.WriteLine("Вторая четверть");
-}
-else if (x < 0 && y < 0)
-{
-    Console.WriteLine("Третья четверть");
-}
-else if (x > 0 && y < 0)
-{
-    Console.WriteLine("Четвертая четверть");
-}
-else
-{
-    Console.WriteLine("Введены не корректные координаты");
-}
+
+string result = Quarter(x, y);
+Console.WriteLine(result);

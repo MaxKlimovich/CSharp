1c3e80be8d8bb5f6b3e3c4be3a3c886453718045
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-14 01:33:09 +0300

    add Home task #23

---
+++ HomeTask#23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N 
+
+Console.Write("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= n)
+{
+    Console.WriteLine($"{count}     |     {Math.Pow(count, 3)}");
+    count++;
+}

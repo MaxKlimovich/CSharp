ebf1a5cfdeec7437a59e1feaee67e57d50645a11
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-13 17:02:14 +0300

    add example 12 EDUCATION METHOD LECTION 3

---
+++ Example012_Method/Program.cs
@@ -0,0 +1,82 @@
+﻿                                        // Вид 1 Методы которые ничего не принимают и ничего не возвращают.
+
+// void Method1();
+// {
+//     Console.WriteLine("Автор ...");
+// }
+// Method1();
+
+                            // Вид 2 Методы которые могут принимать аргументы, но ничего не возвращают.
+
+// void Method2(string msg);
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method21(string msg, int count);
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+//     Method21(msg: "Текст" count: 4);
+
+                                        // Вид 3 Методы которые что-то возвращают но ничего не принимают.
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+                                    // Вид 4 Методы(Функции) которые что-то принимают и что-то возвращают.
+
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(1, "qwerty");
+// Console.WriteLine(res);
+
+
+                                                 // Использование цикла "for" (счетчик который собирает все в одну кучу)
+
+// string Method4(int count, string c)
+// {
+   
+//     string result = String.Empty;
+
+//     for(int i = 0; i < count; i++)
+//     {
+//         result = result + c;
+//     }
+//     return result;
+// }
+// string res = Method4(1, "qwerty");
+// Console.WriteLine(res);
+
+                                                //Ипользование цикла в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+ 
---
+++ HomeTask#19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 → нет
+// 23432 → да
+// 12821 → да
+
+// Если ((число / 10000) == (число % 10) && (число / 1000) == (число % 100)), то это полиндром.
+// Иначе, нет.
+
+Console.WriteLine("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+if ((num / 10000) == (num % 10) && (num / 1000) == (num % 100))
+    Console.WriteLine($"{num} -> да");
+else
+    Console.WriteLine($"{num} -> нет");
+
+Console.ReadLine();
+
+// int Pol(int num)
+// {
+//     if ((num / 10000) == (num % 10) && (num / 1000) == (num % 100))
+//     Console.WriteLine($"{num} -> да");
+// else
+//     Console.WriteLine($"{num} -> нет");
+// }
+
+// int pol = Pol(num); 
+// string result = pol ($"{num > 99999 || num < 9999}") ? pol.ToString() : "не является корректным числом";
+// Console.WriteLine($" {num} => ");

11c36a642663dadc2852d90ec7ca04282dc47f9d
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-15 16:48:27 +0300

    add Practice 012 sum in method

---
+++ Practice#012_Example24/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Задача 24: Напишите программу.
+// 1. Принимает на вход число (А).
+// 2. Выдаёт сумму чисел от 1 до А.
+//
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+Console.WriteLine("Введите любое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int sum = 0;
+
+for (int i = 1; i <= num ; i++)
+{
+    sum = i + sum;
+}
+
+Console.WriteLine(sum);

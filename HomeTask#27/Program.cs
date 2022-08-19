21577ada988a92cb60024cf6a3143d3b2dc1995d
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-19 17:47:45 +0300

    add HomeTask # 27

---
+++ HomeTask#27/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача №27.
+// Напишите программу, которая:
+// 1.Принимает на вход число.
+// 2.Выдает сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number = number /= 10;
+}
+
+Console.WriteLine(sum);

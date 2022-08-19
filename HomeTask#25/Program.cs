3304c961c5168f092baf7397394d87ddbc73885e
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-19 17:08:17 +0300

    add HomeTask#25 in cicle FOR

--- HomeTask#25/Program.cs
+++ HomeTask#25/Program.cs
@@ -4,13 +4,35 @@
 // 1. Принимает на вход 2 числа A и B.
 // 2. Возводит число A в натуральную степень B.
 
+//С помощью метода
+
+// Console.WriteLine("Напишите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Напишите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int x = A;
+// int stepen = B;
+
+// Console.WriteLine("Число {A} степени {B} => " + Math.Pow(x, stepen));
+
+// С помощью метода
+
 
 Console.WriteLine("Напишите число A");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Напишите число B");
 int B = Convert.ToInt32(Console.ReadLine());
 
-int x = A;
-int stepen = B;
 
-Console.WriteLine("A * B = " + Math.Pow(x, stepen));
+int n = A;
+int n2 = B;
+int pow = 1;
+
+for (int i = 0; i < n2; i++)
+{
+    pow = (pow * n);
+} 
+
+
+Console.WriteLine($"Число {A} степени {B} => {pow} ({n}){n2} ");

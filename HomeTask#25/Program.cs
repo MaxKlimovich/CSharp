a7df3d960d08d04e99fcf8c3a62a95bcf23b5bb0
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-19 16:02:17 +0300

    add HomeTask #25

---
+++ HomeTask#25/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+// Напишите цикл который:
+// 1. Принимает на вход 2 числа A и B.
+// 2. Возводит число A в натуральную степень B.
+
+
+Console.WriteLine("Напишите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int x = A;
+int stepen = B;
+
+Console.WriteLine("A * B = " + Math.Pow(x, stepen));

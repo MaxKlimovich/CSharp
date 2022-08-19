bf7b64f3fe706750e7916d9326c4893fbb2bf3ad
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-19 15:02:35 +0300

    add fix #015, fix add []

--- Practice#015_Task30/Program.cs
+++ Practice#015_Task30/Program.cs
@@ -20,6 +20,8 @@ void PrintArr(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
     }
 }

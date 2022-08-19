9714fdaafc59119b3c802aa4222e0ea0b680a882
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-19 15:12:17 +0300

    add second variant Practice #015

--- Practice#015_Task30/Program.cs
+++ Practice#015_Task30/Program.cs
@@ -2,6 +2,32 @@
 // 1.Выводит массив из 8 элементов заполненный нулями и еденицами в случайном порядке.
 
 
+// int[] result = FillArray(8, 0, 2);
+
+// PrintArr(result);
+
+// int[] FillArray(int n, int min, int max)
+// {
+//     int[] array = new int[n];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max);
+//     }
+//     return array;
+// }
+
+// void PrintArr(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == 0) Console.Write("[");
+//         if (i < array.Length - 1) Console.Write(array[i] + ",");
+//         else Console.Write(array[i] + "]");
+//     }
+// }
+
+                                        // second variant
+
 int[] result = FillArray(8, 0, 2);
 
 PrintArr(result);
@@ -18,10 +44,11 @@ int[] FillArray(int n, int min, int max)
 
 void PrintArr(int[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i == 0) Console.Write("[");
         if (i < array.Length - 1) Console.Write(array[i] + ",");
-        else Console.Write(array[i] + "]");
     }
+    Console.Write(array[array.Length -1]);
+    Console.Write("]");
 }

ea2bcbb13bc18420d9a94aedb9e0b6931166dfc2
MaxKlimovich <Klimovich.maks@gmail.com>
2022-08-24 23:39:48 +0300

    add Practice 24 task #42

---
+++ Practice024_DecimalInBinary_Task#42/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// Задача 42: 
+// 1. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Пример:
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecimToBinary(int num)
+{
+    int result = 0;
+    int factor = 1;
+
+    while (num > 0)
+    {
+        result += num % 2*factor;
+        num /= 2;
+        factor *= 10;
+    }
+    return result;
+}
+int res = DecimToBinary(number);
+Console.WriteLine($"{number} -> {res}");
